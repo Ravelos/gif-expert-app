81057f799a534c5ba40a9a466006caf5c5d55589
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-06-06 21:12:34 -0300

    Declares the props required

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,10 +1,15 @@
+import PropTypes from "prop-types";
 
-export const GifItem = ({ title, url, id}) => {
-    
+export const GifItem = ({ title, url, id }) => {
   return (
     <div className="card">
-        <img src={ url } alt={title} />
-        <p>{title}</p>
+      <img src={url} alt={title} />
+      <p>{title}</p>
     </div>
-  )
-}
+  );
+};
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
