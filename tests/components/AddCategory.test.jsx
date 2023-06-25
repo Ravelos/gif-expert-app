2ea538d1913cd861477a4916794ba9db7ff7816d
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-06-25 16:51:35 -0300

    Tests onNewCategory function

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -11,12 +11,23 @@ describe("Testing AddCategory", () => {
 
   test("should call onNewCategory if input has a value", () => {
     const inputValue = "Goku";
-    render(<AddCategory onNewCategory={() => {}} />);
+    const onNewCategory = jest.fn();
+    render(<AddCategory onNewCategory={onNewCategory} />);
     const input = screen.getByRole("textbox");
     const form = screen.getByRole("form");
     fireEvent.input(input, { target: { value: inputValue } });
     fireEvent.submit(form);
     expect(input.value).toBe('');
-
+    expect(onNewCategory).toHaveBeenCalled();
+    expect(onNewCategory).toHaveBeenCalledTimes(1);
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue);
   });
+
+  test('should not call onNewCategory if input value is empty', () => { 
+    const onNewCategory = jest.fn();
+    render(<AddCategory onNewCategory={onNewCategory} />);
+    const form = screen.getByRole("form");
+    fireEvent.submit(form);
+    expect(onNewCategory).toHaveBeenCalledTimes(0);
+   })
 });
