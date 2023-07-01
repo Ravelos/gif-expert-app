cf1519d37149d972fbcf494afa13bb89420d8a7d
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-07-01 16:20:43 -0300

    Uses proptypes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
-import { useFetchGifs } from "../hooks/useFetchGifs";
-import { GifItem } from "./GifItem";
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifItem } from './GifItem';
+import PropTypes from 'prop-types'
 
 export const GifGrid = ( { category } ) => {
 
@@ -23,3 +24,8 @@ export const GifGrid = ( { category } ) => {
     </>
   );
 };
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
+
+}
